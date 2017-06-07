67f57c1076d5e53d805fe0fe4b8276241bad5ea0
MDP <mike@sdpdigital.com>
2017-06-07 14:25:30 -0400

    Exposed new is form item valid method

--- ResearchKit/Common/ORKFormStepViewController.h
+++ ResearchKit/Common/ORKFormStepViewController.h
@@ -48,6 +48,8 @@ NS_ASSUME_NONNULL_BEGIN
 ORK_CLASS_AVAILABLE
 @interface ORKFormStepViewController : ORKStepViewController
 
+- (BOOL)isFormItemValidFor:(NSString*)formItemIdentifier;
+
 @end
 
 NS_ASSUME_NONNULL_END
